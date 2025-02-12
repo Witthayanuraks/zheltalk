2f9f77e17ef1dc646b683581e334a740677a1f22
Aris Noer Cahya <muharlan882@gmail.com>
2025-02-12 15:32:36 +0700

    Delete backend directory

--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express = require('express')
-const express = require("express");
-const router = express.Router();
-
-router.get("/hello", (req, res) => {
-  res.json({ message: "Tes bekend" });
-});
-
-module.exports = router;
